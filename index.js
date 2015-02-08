9e479a751015f6354dd5d63169042d17d98f1d5d
Out of Ink Software <moshekrum@gmail.com>
2015-02-08 16:43:26 -0500

    added win/losing, bounce angle, speed increase, added todo
    fixed not detecting disconnect from server due to messed up brackets and
    parenthesis

--- index.js
+++ index.js
@@ -2,30 +2,37 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-	res.sendFile('index.html', {root: __dirname});
+app.get('/', function(req, res) {
+    res.sendFile('index.html', {root: __dirname});
 });
 
 var users = 0;
-io.on('connection', function(socket){
-	console.log("client connected");
-	users++;
-	console.log("there are now "+users+" user(s)")
-	if (users % 2 == 0) {
-		socket.broadcast.emit('send ball', {x:400,y:300,vx:8,vy:8,radius:50});
-		console.log("beginning game");
-	}
-  socket.on('send ball', function(msg){
-    console.log('ball: ' + msg.x);
-    socket.broadcast.emit('send ball', msg);
-   socket.on('disconnect', function(){
-    console.log("client disconnected");
-	users--;
-	console.log("there are now "+users+" user(s)")
+io.on('connection', function(socket) {
+    console.log("client connected");
+    users++;
+    console.log("there are now " + users + " user(s)")
+    if (users % 2 == 0) {
+        socket.broadcast.emit('send ball', {x:512,y:468,vx:0,vy:-10,radius:50});
+        console.log("beginning game");
+    }
+    socket.on('send ball', function(msg) {
+        console.log('ball: ' + msg.x);
+        socket.broadcast.emit('send ball', msg);
+    });
+    socket.on('you win', function() {
+	    console.log('win');
+	    setTimeout(function() {
+	        socket.broadcast.emit('send ball', {x:512,y:468,vx:0,vy:-10,radius:50});
+	    }, 1500);
+	    socket.broadcast.emit('win');
 	});
-  });
+    socket.on('disconnect', function() {
+        console.log("client disconnected");
+        users--;
+        console.log("there are now " + users + " user(s)")
+    });
 });
 
-http.listen(process.env.PORT || 80, function(){
-	console.log('listening on *:80');
+http.listen(process.env.PORT || 80, function() {
+    console.log('listening on *:80');
 });
