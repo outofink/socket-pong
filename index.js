8c9d7736387fd25b74145ac2e505e81fa0988ece
Out of Ink Software <moshekrum@gmail.com>
2015-02-17 17:29:33 -0500

    try again

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
     res.sendFile('index.html', {root: __dirname});
 });
+app.use(express.static(__dirname));
 
 var users = 0;
 io.on('connection', function(socket) {
