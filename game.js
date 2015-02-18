abee4309c31c31e891dc290fc6ca968f05b802f9
Out of Ink Software <moshekrum@gmail.com>
2015-02-17 19:45:06 -0500

    code cleanup

--- game.js
+++ game.js
@@ -1,205 +1,211 @@
 var DEBUG = 0;
 
-var socket = io.connect("https://socket-pong.herokuapp.com/");
-var onscreen = false;
+if (DEBUG == 0) var socket = io.connect("https://socket-pong.herokuapp.com/");
+if (DEBUG == 1) var socket = io.connect("http://localhost"); //for testing
 
-if (DEBUG == 1) {
-  var socket = io.connect("http://localhost"); //for testing
-}
 var hudmsg = "";
 
 var score = 0;
 var theirScore = 0;
-var paddle;
-var canvas;
-var deadball = {x:0,y:0,vx:0,vy:0,radius:0};
 
-var ball=deadball;
-socket.on('send ball', function(msg){
-  ball = msg;
-  onscreen = true;
+var onscreen = false;
+
+var paddle = {x:512, y:675, w:250, h:25};
+var canvas = document.getElementById("canvas");
+
+var gameActive = true;
+
+var deadball = {x:0, y:0, vx:0, vy:0, radius:0};
+
+var ball = deadball;
+
+//socket inputs
+socket.on('send ball', function(msg) {
+    ball = msg;
+    onscreen = true;
 });
-socket.on('win', function(msg){
-  hudmsg="You win!";
-  score+=1;
-  setTimeout(function() {
-        hudmsg ="";
-  }, 1500);
+socket.on('win', function(msg) {
+    hudmsg = "You win!";
+    score += 1;
+    setTimeout(function() {
+        hudmsg = "";
+    }, 1500);
 });
 
-initCanvas()
 function initCanvas() {
+    var W = window.innerWidth;
+    var H = window.innerHeight;
 
-  //Initialise array
-  paddle = {x:512, y:675, w:250, h:25};
-  canvas = document.getElementById("canvas");
-
-  var W = window.innerWidth;
-  var H = window.innerHeight;
-
-  canvas.width= W;
-  canvas.height= H;
-  canvas.style.width = W;
-  canvas.style.height = H;
-
-  //Add eventlistener to canvas
-  gameLoop();
-  canvas.addEventListener('touchmove', function() {
-    var touch = event.targetTouches[0];
-
-    if(detectHit(paddle.x, paddle.y, touch.pageX, touch.pageY, paddle.w, paddle.h)) {
-      var buffer = 20;
-      if (((touch.pageX-paddle.w/2 - buffer) > 0) && ((touch.pageX+paddle.w/2 + buffer) < canvas.width)) {
-        paddle.x = touch.pageX;
-      }
-      else if ((touch.pageX-paddle.w/2 - buffer) <= 0) {
-        paddle.x = paddle.w/2 + buffer;
-      }
-      else if ((touch.pageX+paddle.w/2 + buffer) >= canvas.width) {
-        paddle.x = canvas.width - paddle.w/2 - buffer;
-      }
-
-      //canDraw();
-    }
-    event.preventDefault();
-  }, false);
-
-  //canDraw();
+    canvas.width = W;
+    canvas.height = H;
+    canvas.style.width = W;
+    canvas.style.height = H;
 }
 
-function detectHit(x1,y1,x2,y2,w,h) {
-
-  //Very simple detection here, could use distance between two pts e.g., or more complex polygonal bounding boxes
-  if(Math.abs(x2-x1)>w/2) return false;
-  return true;
+//Very simple detection, could use distance between two points, or more complex polygonal bounding boxes
+function detectHit(x1, y1, x2, y2, w, h) {
+    if (Math.abs(x2 - x1) > w / 2) return false;
+    return true;
 }
 
 function canDraw() {
-  //clear canvas
-  canvas = document.getElementById("canvas");
-  var ctx = canvas.getContext('2d');
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //clear canvas
+    canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //ball
+    ctx.beginPath();
+    ctx.fillStyle = "black";
+    ctx.arc(ball.x, ball.y, (ball.radius), Math.PI * 2, false);
+    ctx.fill();
+    //debug info
+    if (DEBUG == 1) {
+        if (onscreen == false) {
+            ctx.fillStyle = "red";
+        }
+        ctx.font = "bold 16px Arial";
+        ctx.textAlign = "left";
+        ctx.fillText(JSON.stringify(ball), 50, canvas.height - 50);
+    }
+    //paddle
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(paddle.x - paddle.w / 2, paddle.y - paddle.h / 2, paddle.w, paddle.h);
+    //hud message
+    ctx.font = "bold 108px Arial";
+    ctx.textAlign = "center";
+    ctx.fillStyle = 'black';
+    ctx.fillText(hudmsg, canvas.width / 2, canvas.height / 2);
+    //score
+    ctx.font = "bold 72px Arial";
+    ctx.textAlign = "left";
+    ctx.fillText(score, 25, 75);
+    //theirScore
+    ctx.font = "bold 32px Arial";
+    ctx.textAlign = "left";
+    ctx.fillText(theirScore, 75, 50);
+}
 
-  p=ball;
+function canUpdate() {
+    //bounce off left or right side
+    if ((ball.x + ball.radius) > canvas.width) {
 
-  if ((p.x + p.radius) > canvas.width) {
+        ball.x = canvas.width - ball.radius;
+        ball.vx *= -1;
+    } 
+    else if ((ball.x - ball.radius) < 0) {
+        ball.x = ball.radius;
+        ball.vx *= -1;
+    }
+    //hit the bottom
+    if ((ball.y + ball.radius) > canvas.height) {
 
-      p.x = canvas.width - p.radius;
-      p.vx *= -1;
-  }
-  else if ((p.x - p.radius) < 0) {
-      p.x = p.radius;
-      p.vx *= -1;
-  }
-
-  if ((p.y + p.radius) > canvas.height) {
-      
-      hudmsg ="You lose!";
-      socket.emit('you win');
-      theirScore+=1;
-      setTimeout(function() {
-        hudmsg ="";
-      }, 1500);
-      onscreen = false;
-      ball=deadball;
-
-  }
-
-  else if ((p.y < -100) && (p.vy < 0)) {
-      //p.y = -100;
-      if (onscreen) {
-        socket.emit('send ball', {x:canvas.width-p.x, y:p.y, vx:-p.vx, vy:-p.vy, radius:p.radius});
+        hudmsg = "You lose!";
+        socket.emit('you win');
+        theirScore += 1;
+        setTimeout(function() {
+            hudmsg = "";
+        }, 1500);
         onscreen = false;
-      }
-      ball=deadball;
+        ball = deadball;
 
-  }
-//   (     ball and paddle overlap on y-axis     )    (  ball and paddle overlap on x-axis   )   (going down)   (                 not too overlapped                 )
-  if (((p.y+p.radius) - (paddle.y-paddle.h/2) > 0) && (Math.abs(p.x - paddle.x) <= paddle.w/2+15) && (p.vy > 0) && ((p.y+p.radius) - (paddle.y-paddle.h/2) < p.radius))  {
-    p.vy *= -1;
-        dist = paddle.y - p.y;
-        minDist = paddle.h/2 + p.radius;
-        var dx = paddle.x - p.x;
-        var dy = paddle.y - p.y;
+    }
+    //hit the top 
+    else if ((ball.y < -100) && (ball.vy < 0)) {
+        //ball.y = -100;
+        if (onscreen) {
+            socket.emit('send ball', {
+                x: canvas.width - ball.x,
+                y: ball.y,
+                vx: -ball.vx,
+                vy: -ball.vy,
+                radius: ball.radius
+            });
+            onscreen = false;
+        }
+        ball = deadball;
 
-        var tx = p.x + ((dx / dist) * minDist);
-        var ty = p.y + ((dy / dist) * minDist);
-        console.log(dist);
-        console.log(minDist);
-        console.log(tx);
-        console.log(ty);
+    }
+    // hit the paddle (a bit experimental...)
+    if (
+      ((ball.y + ball.radius) - (paddle.y - paddle.h / 2) > 0) && // ball and paddle overlap on y-axis
+      (Math.abs(ball.x - paddle.x) <= paddle.w / 2 + 15) && // ball and paddle overlap on x-axis
+      ((ball.y + ball.radius) - (paddle.y - paddle.h / 2) < ball.radius) && //not too overlapped
+      (ball.vy > 0) // going down
+    ) { 
+        ball.vy *= -1;
+        dist = paddle.y - ball.y;
+        minDist = paddle.h / 2 + ball.radius;
+        var dx = paddle.x - ball.x;
+        var dy = paddle.y - ball.y;
+
+        var tx = ball.x + ((dx / dist) * minDist);
+        var ty = ball.y + ((dy / dist) * minDist);
+
         var ax = (tx - paddle.x);
         var ay = (ty - paddle.y);
-        
-        var OLDpvx = p.vx;
-        var OLDpvy = p.vy;
-        
-        p.x -= ax;
-        p.y -= ay;
-        
-        
-        p.vx -= ax;
-        p.vy -= ay;
 
-        var ratio = Math.sqrt(((OLDpvx * OLDpvx) + (OLDpvy * OLDpvy))) / Math.sqrt(((p.vx * p.vx) +  (p.vy * p.vy)));
-        p.vx *= ratio;
-        p.vy *= ratio;
+        var OLDpvx = ball.vx;
+        var OLDpvy = ball.vy;
+
+        ball.x -= ax;
+        ball.y -= ay;
 
 
-        if (Math.sqrt(((p.vy * p.vy) + (p.vx * p.vx))) < 30) {
-          p.vx*=1.05;
-          p.vy*=1.05;
+        ball.vx -= ax;
+        ball.vy -= ay;
+
+        var ratio = Math.sqrt(((OLDpvx * OLDpvx) + (OLDpvy * OLDpvy))) / Math.sqrt(((ball.vx * ball.vx) + (ball.vy * ball.vy)));
+        ball.vx *= ratio;
+        ball.vy *= ratio;
+
+        //speed up the ball (but not too fast...)
+        if (Math.sqrt(((ball.vy * ball.vy) + (ball.vx * ball.vx))) < 30) {
+            ball.vx *= 1.05;
+            ball.vy *= 1.05;
         }
-  }
-  p.x += p.vx;
-  p.y += p.vy;
-  //ball
-  ctx.beginPath();
-  ctx.fillStyle = "black";
-  ctx.arc(p.x, p.y, (p.radius), Math.PI*2, false);
-  ctx.fill();
-  //debug info
-  if (DEBUG == 1) {
-    if (onscreen == false) {ctx.fillStyle="red";}
-    ctx.font = "bold 16px Arial";
-    ctx.textAlign="left";
-    ctx.fillText(JSON.stringify(ball), 50, canvas.height-50);
-  }
-  //paddle
-  ctx.fillStyle = 'blue';
-  ctx.fillRect(paddle.x - paddle.w/2, paddle.y- paddle.h/2, paddle.w, paddle.h);
-  //hud message
-  ctx.font = "bold 108px Arial";
-  ctx.textAlign="center";
-  ctx.fillStyle = 'black';
-  ctx.fillText(hudmsg, canvas.width/2, canvas.height/2);
-  //score
-  ctx.font = "bold 72px Arial";
-  ctx.textAlign="left";
-  ctx.fillText(score, 25, 75);
-  //theirScore
-  ctx.font = "bold 32px Arial";
-  ctx.textAlign="left";
-  ctx.fillText(theirScore, 75, 50);
+    }
+    //standard updating
+    ball.x += ball.vx;
+    ball.y += ball.vy;
 
-} 
-
+}
 var fps = 60;
 var now;
 var then = Date.now();
-var interval = 1000/fps;
+var interval = 1000 / fps;
 var delta;
 
+initCanvas()
+
 function gameLoop() {
-  requestAnimationFrame(gameLoop);
-     
+    requestAnimationFrame(gameLoop);
+
     now = Date.now();
     delta = now - then;
-     
+
     if (delta > interval) {
-      then = now - (delta % interval);
-
-      canDraw();
-
-  }
+        then = now - (delta % interval);
+        if (gameActive) {
+            canUpdate();
+            canDraw();
+        }
+    }
 };
+
+canvas.addEventListener('touchmove', function() {
+    var touch = event.targetTouches[0];
+
+    if (detectHit(paddle.x, paddle.y, touch.pageX, touch.pageY, paddle.w, paddle.h)) {
+        var buffer = 20;
+        if (((touch.pageX - paddle.w / 2 - buffer) > 0) && ((touch.pageX + paddle.w / 2 + buffer) < canvas.width)) {
+            paddle.x = touch.pageX;
+        } else if ((touch.pageX - paddle.w / 2 - buffer) <= 0) {
+            paddle.x = paddle.w / 2 + buffer;
+        } else if ((touch.pageX + paddle.w / 2 + buffer) >= canvas.width) {
+            paddle.x = canvas.width - paddle.w / 2 - buffer;
+        }
+    }
+    event.preventDefault();
+}, false);
+
+gameLoop();
